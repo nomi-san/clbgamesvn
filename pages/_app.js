c90b3a7a034cab42e661a50e43d170997afc7ae2
nomi-san <wuuyi123@gmail.com>
2020-12-30 16:17:15 +0700

    Add styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 
+import 'normalize.css'
+import 'metro4/build/css/metro.min.css'
+import '../styles/main.css'
+
 const App = ({ Component, pageProps }) => {
     return (
         <Component {...pageProps} />
